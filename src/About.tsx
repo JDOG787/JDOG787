7532d024131875042c5ebc2c9f0d7e16af6aa8e3
JDOG787 <redstoneexpert787@icloud.com>
2021-02-20 22:19:02 -0700

    work on about page

--- src/About.tsx
+++ src/About.tsx
@@ -5,9 +5,16 @@ const About: React.FC = () => {
     return (
         <div className="about">
             <div className="terminal">
-                <div className="current-dir">jdog787/about.txt <span className="end-current-dir">$</span></div>
+                <div className="current-dir">jdog787/about.txt <span className="end-current-dir">$</span> _</div>
+                <div className="terminal-text">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <p>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
             </div>
-            {/* <h1 className="about-header">jdog/about <span className="terminal">$</span></h1> */}
         </div>
     )
 }

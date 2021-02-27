abf4b92c561fcb28831f1ecdbc8c5cd3c3db4933
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2021-02-27 12:14:22 -0700

    fix typing thing

--- src/Work.tsx
+++ src/Work.tsx
@@ -8,17 +8,33 @@ const Work: React.FC = () => {
             <Header mainHeader="My work"/>
             <p className="sub-header">Check out some of my projects</p>
             <div className="projects">
-                <div className="project">
-                    <h2>Name of project</h2>
-                    <p className="project-info">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempore est nam?
-                    </p>
+                <div className="col">
+                    <div className="project">
+                        <h2>Name of project</h2>
+                        <p className="project-info">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempore est nam?
+                        </p>
+                    </div>
+                    <div className="project">
+                        <h2>Name of project</h2>
+                        <p className="project-info">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempore est nam?
+                        </p>
+                    </div>
                 </div>
-                <div className="project">
-                    <h2>Name of project</h2>
-                    <p className="project-info">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempore est nam?
-                    </p>
+                <div className="col">
+                    <div className="project">
+                        <h2>Name of project</h2>
+                        <p className="project-info">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempore est nam?
+                        </p>
+                    </div>
+                    <div className="project">
+                        <h2>Name of project</h2>
+                        <p className="project-info">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tempore est nam?
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
